5906043f85430661d8bf37793240ffa8d8fbed82
Thibault <thibault.manchon@hotmail.fr>
2020-08-26 13:07:11 +0200

    cloudinary setup, signup form with username and form design

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.12.0'
 
 gem 'faker'
 gem 'pundit'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :online])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
   end
 
   # Pundit: white-list approach.
