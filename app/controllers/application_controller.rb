71015ad8aa3a636507a61ffe935d08920d1b5d5e
Thibault <thibault.manchon@hotmail.fr>
2020-08-27 17:03:04 +0200

    first try of implementation of online button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :online])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :online])
   end
 
   # Pundit: white-list approach.
