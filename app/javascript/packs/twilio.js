9fc63f0470761b39819e9e708e2ab2df4b23264a
Manal Malik <manalmalik.cs@gmail.com>
2020-08-28 15:16:28 +0200

    Implement end call functionality

--- app/javascript/packs/twilio.js
+++ app/javascript/packs/twilio.js
@@ -2,117 +2,90 @@ const twilio = () => {
 
   const btn = document.getElementById("connect-button")
   const share = document.getElementById("share-button")
+  const endCall = document.getElementById("end-button")
   const user_token = document.getElementById("user-token")
+
   if (btn) {
   const token_1 = user_token.innerHTML
-// console.log(token_1)
   const { connect, LocalVideoTrack, createLocalTracks} = require('twilio-video');
-
-
 
   btn.addEventListener('click', event => {
     event.preventDefault
-    //const { connect, createLocalTracks } = require('twilio-video');
-  VideoCall()
-    async function VideoCall(){
+    VideoCall()
+      async function VideoCall(){
 
-
-    // createLocalTracks({
-    //   audio: true,
-    //   video: { width: 640 }
-    // }).then(localTracks => {
-    //   return connect(token_1, { name:'my-new-room' }).then(room => {
-    //     console.log(`Successfully joined a Room: ${room.name}`);
-    //     room.on('participantConnected', participant => {
-    //       console.log(`Participant "${participant.identity}" connected`);
-    //       participant.tracks.forEach(publication => {
-    //         if (publication.isSubscribed) {
-    //           const track = publication.track;
-    //           document.getElementById('remote-media-div').appendChild(track.attach());
-    //         }
-    //       });
-    //       participant.on('trackSubscribed', track => {
-    //         document.getElementById('remote-media-div').appendChild(track.attach());
-    //       });
-    //     });
-
-    //     console.log(room.participants)
-    //     room.participants.forEach(participant => {
-    //       participant.tracks.forEach(publication => {
-    //         if (publication.track) {
-    //           document.getElementById('remote-media-div').appendChild(publication.track.attach());
-    //         }
-    //       });
-    //       participant.on('trackSubscribed', track => {
-    //         document.getElementById('remote-media-div').appendChild(track.attach());
-    //       });
-    //     });
-    //   }, error => {
-    //     console.error(`Unable to connect to Room: ${error.message}`);
-    //   });
-    // })
-
-
-    const room = await connect(token_1, {
-      audio: true,
-      name: 'my-room-name',
-      video: { width: 640}
-    });
-
-   // Connecting Participant
-    room.on('participantConnected', participant => {
-    console.log(`Participant "${participant.identity}" connected`);
-
-      participant.tracks.forEach(publication => {
-        if (publication.isSubscribed) {
-          const track = publication.track;
-          document.getElementById('remote-media-div').appendChild(track.attach());
-        }
+      const room = await connect(token_1, {
+        audio: true,
+        name: 'my-room-name',
+        video: { width: 640}
       });
 
-      participant.on('trackSubscribed', track => {
-        document.getElementById('remote-media-div').appendChild(track.attach());
-      });
-    });
+     // Connecting Participant
+      room.on('participantConnected', participant => {
+      console.log(`Participant "${participant.identity}" connected`);
 
-    // Remote Participant
-    function RemoteParticipant(){
-        room.participants.forEach(participant => {
-          participant.tracks.forEach(publication => {
-            if (publication.track) {
-              document.getElementById('remote-media-div').appendChild(publication.track.attach());
-            }
-          });
+        participant.tracks.forEach(publication => {
+          if (publication.isSubscribed) {
+            const track = publication.track;
+            document.getElementById('remote-media-div').appendChild(track.attach());
+          }
+        });
 
-       participant.on('trackSubscribed', track => {
+        participant.on('trackSubscribed', track => {
           document.getElementById('remote-media-div').appendChild(track.attach());
         });
       });
-    }
 
-      RemoteParticipant();
+      // Remote Participant
+      function RemoteParticipant(){
+          room.participants.forEach(participant => {
+            participant.tracks.forEach(publication => {
+              if (publication.track) {
+                document.getElementById('remote-media-div').appendChild(publication.track.attach());
+              }
+            });
 
-        var Video = require('twilio-video');
-          // Request audio and video tracks
-        Video.createLocalTracks().then(function(localTracks) {
-            var localMediaContainer = document.getElementById('local-media-container-id');
-            localTracks.forEach(function(track) {
-            localMediaContainer.appendChild(track.attach());
+         participant.on('trackSubscribed', track => {
+            document.getElementById('remote-media-div').appendChild(track.attach());
           });
-      });
-
-
-
-      share.addEventListener('click', event => {
-        async function startCapture() {
-        const stream = await navigator.mediaDevices.getDisplayMedia();
-        const screenTrack = new LocalVideoTrack(stream.getTracks()[0]);
-        room.localParticipant.publishTrack(screenTrack);
+        });
       }
-        startCapture();
-      });
-    }
-  });
+
+        RemoteParticipant();
+
+          var Video = require('twilio-video');
+            // Request audio and video tracks
+          Video.createLocalTracks().then(function(localTracks) {
+              var localMediaContainer = document.getElementById('local-media-container-id');
+              localTracks.forEach(function(track) {
+              localMediaContainer.appendChild(track.attach());
+            });
+        });
+
+
+
+        share.addEventListener('click', event => {
+          async function startCapture() {
+          const stream = await navigator.mediaDevices.getDisplayMedia();
+          const screenTrack = new LocalVideoTrack(stream.getTracks()[0]);
+          room.localParticipant.publishTrack(screenTrack);
+        }
+          startCapture();
+        });
+
+        endCall.addEventListener('click', event => {
+          room.localParticipant.tracks.forEach(publication => {
+            const attachedElements = publication.track.detach();
+            attachedElements.forEach(element => element.remove());
+          });
+
+          room.disconnect();
+          window.location.replace("/");
+        });
+      }
+    });
+
+
 }
 
 
