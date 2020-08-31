35972345f1513568aefaa46737c356840432fbe0
Arthur Bizet <arthur.m.bizet@gmail.com>
2020-08-31 18:18:09 +0200

    input field empty after posting message

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -11,6 +11,9 @@ const initChatCable = () => {
         // console.log(data);
         messagesContainer.insertAdjacentHTML('beforeend', data);
 
+        const inputMessage = document.getElementById('message_content');
+        inputMessage.value = "";
+
         console.log(window.location.href)
         const regex = RegExp('categories\/\d+\/tickets\/\d+\/chats\/\d+');
         if (!regex.test(window.location.href)) {
