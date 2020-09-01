619085b725c343de528733da6f7804100eec9455
Thibault <thibault.manchon@hotmail.fr>
2020-09-01 14:57:07 +0200

    action text implementted

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,8 @@ class Ticket < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :chats, dependent: :destroy
+  has_rich_text :description
+
   validates :description, presence: true
   validates :category, presence: true
   validates :title, presence: true
