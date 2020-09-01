ee078742438c25a1803a4964d22564a23454bd21
Manal Malik <manalmalik.cs@gmail.com>
2020-09-01 18:21:04 +0200

    Add call duration

--- app/javascript/packs/twilio.js
+++ app/javascript/packs/twilio.js
@@ -122,12 +122,15 @@ const twilio = () => {
           window.location.replace("/");
         });
       }
+        // Start timer for call
+    var sec = 0;
+    function pad ( val ) { return val > 9 ? val : "0" + val; }
+    setInterval( function(){
+        document.getElementById("seconds").innerHTML=pad(++sec%60);
+        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+    }, 1000);
     });
-
-
 }
-
-
 
 }
 export { twilio }
