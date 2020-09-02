0d379ac5ea237636e08b0d77f21d91aa7e5c9ffb
Thibault <thibault.manchon@hotmail.fr>
2020-09-02 18:38:59 +0200

    minor colour fix

--- app/javascript/packs/availibility.js
+++ app/javascript/packs/availibility.js
@@ -4,10 +4,10 @@ const availibility = () => {
   const action = document.querySelector('.availibity');
   action.addEventListener('click', () => {
     const dot = document.querySelector('.status');
-    if(dot.style.backgroundColor == 'green') {
-      dot.style.backgroundColor = '#A82929';
+    if(dot.style.backgroundColor == '#299a0b') {
+      dot.style.backgroundColor = '#ff3019';
     } else {
-      dot.style.backgroundColor = 'green';
+      dot.style.backgroundColor = '#299a0b';
     }
   });
 };
