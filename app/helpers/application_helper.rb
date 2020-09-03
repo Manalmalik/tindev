17942ed27628a3ae2459291bc613fefc504aaeb4
Thibault <thibault.manchon@hotmail.fr>
2020-09-03 12:52:28 +0200

    navbar fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,8 @@
 module ApplicationHelper
-   def resource_name
-    :user
+
+  def current_class?(test_path)
+    return 'active' if request.path == test_path
+    ''
   end
-  def resource
-    @resource ||= User.new
-  end
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
+
 end
