c7b5f193e4f43959d0a2295fbc3738c121d2b763
Thibault <thibault.manchon@hotmail.fr>
2020-09-03 16:56:47 +0200

    last fixeeees

--- app/javascript/packs/availibility.js
+++ app/javascript/packs/availibility.js
@@ -4,10 +4,16 @@ const availibility = () => {
   const action = document.querySelector('.availibity');
   action.addEventListener('click', () => {
     const dot = document.querySelector('.status');
+    const status = document.querySelector('.availibity');
     if(dot.style.backgroundColor == 'rgb(41, 154, 11)') {
       dot.style.backgroundColor = '#ff3019';
+      status.innerHTML ='Offline';
+      status.style.color = '#ff3019';
+
     } else {
       dot.style.backgroundColor = 'rgb(41, 154, 11)';
+      status.innerHTML ='Online';
+      status.style.color = 'rgb(41, 154, 11)';
     }
   });
 };
